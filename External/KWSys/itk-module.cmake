c45e6ae35298fed73412b37b43dc6d6a5e8ebe2e
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-01-30 17:29:38 +0100

    Tried another path to see if things work.

--- External/KWSys/itk-module.cmake
+++ External/KWSys/itk-module.cmake
@@ -14,7 +14,8 @@ itk_module(ITKKWSys
 set(${itk-module}-targets ${itk-module}Targets)
 set(${itk-module}-targets-install "\${ITK_INSTALL_PREFIX}/${ITK_INSTALL_PACKAGE_DIR}/Modules/Targets/${itk-module}Targets.cmake")
 set(${itk-module}_TARGETS_FILE_INSTALL "${${itk-module}-targets-install}")
-set(${itk-module}-targets-build-directory "${ITK_DIR}/${ITK_INSTALL_PACKAGE_DIR}/Modules/Targets")
+#set(${itk-module}-targets-build-directory "${ITK_DIR}/${ITK_INSTALL_PACKAGE_DIR}/Modules/Targets")
+set(${itk-module}-targets-build-directory "${CTK_SOURCE_DIR}/${ITK_INSTALL_PACKAGE_DIR}/Modules/Targets")
 file(MAKE_DIRECTORY ${${itk-module}-targets-build-directory})
 set(${itk-module}-targets-build "${${itk-module}-targets-build-directory}/${itk-module}Targets.cmake")
 
