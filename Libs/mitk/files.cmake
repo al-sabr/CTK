0531b29d43775862080f2e357b839c59d375dab7
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-02-08 15:51:44 +0100

    Created a new internal library for MITK related files.

---
+++ Libs/mitk/files.cmake
@@ -0,0 +1,14 @@
+set(KIT_MOC_SRCS
+  # Adding MITK logging system headers.
+  mitkLog.h
+  mitkLogMacros.h
+  mitkException.h
+  mitkExceptionMacros.h
+
+)
+
+set(KIT_SRCS
+  mitkException.cpp
+  mitkLog.cpp
+
+)
