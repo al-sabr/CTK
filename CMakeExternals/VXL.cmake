e9bb9c26f522aa1a15f19bb14075736a2f762a11
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-02-09 20:56:13 +0100

    Adding new external dependencies for KWSys & VXL.

---
+++ CMakeExternals/VXL.cmake
@@ -0,0 +1,64 @@
+#
+# VXL
+#
+
+set(proj VXL)
+
+set(${proj}_DEPENDENCIES "")
+
+ExternalProject_Include_Dependencies(${proj}
+  PROJECT_VAR proj
+  DEPENDS_VAR ${proj}_DEPENDENCIES
+  EP_ARGS_VAR ${proj}_EXTERNAL_PROJECT_ARGS
+  USE_SYSTEM_VAR ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj}
+  )
+
+if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+  message(FATAL_ERROR "Enabling ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj} is not supported !")
+endif()
+
+# Sanity checks
+if(DEFINED VLX_DIR AND NOT EXISTS ${ZMQ_DIR})
+  message(FATAL_ERROR "VLX_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+if(NOT DEFINED VLX_DIR)
+
+  set(revision_tag faa3fcf45bb6c4374431b42bc3b5ad9ce518dbd8)
+  if(${proj}_REVISION_TAG)
+    set(revision_tag ${${proj}_REVISION_TAG})
+  endif()
+
+  set(location_args )
+  if(${proj}_URL)
+    set(location_args URL ${${proj}_URL})
+  elseif(${proj}_GIT_REPOSITORY)
+    set(location_args GIT_REPOSITORY ${${proj}_GIT_REPOSITORY}
+                      GIT_TAG ${revision_tag})
+  else()
+    set(location_args GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/vxl/vxl.git"
+                      GIT_TAG ${revision_tag})
+  endif()
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EXTERNAL_PROJECT_ARGS}
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BINARY_DIR ${proj}-build
+    PREFIX ${proj}${ep_suffix}
+    ${location_args}
+    INSTALL_COMMAND ""
+    CMAKE_CACHE_ARGS
+      ${ep_common_cache_args}
+     DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+  set(VLX_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+
+mark_as_superbuild(
+  VARS VLX_DIR:PATH
+  LABELS "FIND_PACKAGE"
+  )
