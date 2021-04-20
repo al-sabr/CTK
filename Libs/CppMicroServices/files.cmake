dbfd0b13e9ce0c082385971ca4286a5a1db299bd
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-04-20 02:09:07 +0200

    Initial import of some files from CppMicroServices 2.9.9 for porting plan with the latest version of CppMicroServices 3.6.0

---
+++ Libs/CppMicroServices/files.cmake
@@ -0,0 +1,12 @@
+set(KIT_MOC_SRCS
+  mitkBaseApplication.h
+  QmitkSafeApplication.h
+  QmitkSingleApplication.h
+)
+
+set(KIT_SRCS
+  mitkBaseApplication.cpp
+  mitkProvisioningInfo.cpp
+  QmitkSafeApplication.cpp
+  QmitkSingleApplication.cpp
+)
